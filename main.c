87c313ff879f1ebe89b4d16850b8189cedb096c3
parth <parthbhutada13@gmail.com>
2025-12-16 02:08:12 +0530

    Add includes and define constants for rods

--- main.c
+++ main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+#define MAX 5
+#define RODS 3
+#define BASE_WIDTH (MAX * 2 + 1)
+
+int rods[RODS][MAX];
+int top[RODS];
+int n;
+int moves = 0;
+int maxMoves;
